6a5d81bf8bbfa79cec8bbd3193e9b0b4b2c5c10a
xx1064xx <xx1064xx@gmail.com>
2024-01-18 18:52:41 +0100

    update 0.1

--- Program.cs
+++ Program.cs
@@ -1,4 +1,12 @@
+using Microsoft.AspNetCore.StaticFiles;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllers();
+
+var provider = new FileExtensionContentTypeProvider();
+provider.Mappings[".js"] = "application/javascript";
+provider.Mappings[".css"] = "text/css";
 
 // Add services to the container.
 builder.Services.AddRazorPages();
@@ -14,12 +22,26 @@ if (!app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
-app.UseStaticFiles();
+app.UseStaticFiles(new StaticFileOptions
+{
+    ContentTypeProvider = provider,
+    OnPrepareResponse = context =>
+    {
+        if (!context.File.Name.EndsWith(".html")) // Hier anpassen für die Dateitypen, die du bearbeiten möchtest
+        {
+            context.Context.Response.Headers["Cache-Control"] = "no-cache, no-store";
+            context.Context.Response.Headers["Pragma"] = "no-cache";
+            context.Context.Response.Headers["Expires"] = "-1";
+        }
+    }
+});
 
 app.UseRouting();
 
 app.UseAuthorization();
 
 app.MapRazorPages();
+
+app.MapControllers();
 
 app.Run();
