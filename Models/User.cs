0c60b72f4853aada88579d7317613ae7d4f4ebf7
xx1064xx <xx1064xx@gmail.com>
2024-01-20 00:14:46 +0100

    aklsjd

---
+++ Models/User.cs
@@ -0,0 +1,11 @@
+﻿namespace AzoreMessanger.Models
+{
+    public class User
+    {
+        public long Id { get; set; }
+        public string? username { get; set; }
+        public string? email { get; set; }
+        public string? password { get; set; }
+        public string? Salt { get; set; }
+    }
+}
