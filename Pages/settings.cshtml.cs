0a338501f740fece7ee43a679dd049b160bff0ac
xx1064xx <xx1064xx@gmail.com>
2024-01-19 20:24:51 +0100

    V0.2

---
+++ Pages/settings.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace AzoreMessanger.Pages
+{
+    public class settingModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
