757126d7da66f379988cdeee07e7b59521bcfbf7
xx1064xx <xx1064xx@gmail.com>
2024-01-24 22:35:06 +0100

    notfall gelöst

--- Controller/browserController.cs
+++ Controller/browserController.cs
@@ -3,6 +3,7 @@ using AzoreMessanger.Models;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Data.SqlClient;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.IdentityModel.Tokens;
 using System;
@@ -41,18 +42,58 @@ namespace AzoreMessanger.Controller
         {
 
             // rechnet die tiefstverfügbare Browsernummer aus (Für bessere Darstellung)
-            int minUnusedBrowserNumber = 1;
+            int minBrowserNummer = 0;
+
+            string connectionString = "Data Source=(LocalDB)\\MSSQLLocalDB;AttachDbFilename=C:\\Users\\Jusa\\Documents\\asp.net\\WebApplication1\\Data\\azoreDb.mdf;Integrated Security=True;Connect Timeout=30";
+
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+
+                long x = browserInfo.userId;
+
+                // SQL-Abfrage, um die niedrigste unbenutzte Browsernummer für UserID 2 zu erhalten
+                string sqlQuery = $@"
+        SELECT TOP 1 n
+        FROM (
+            SELECT 1 AS n
+            UNION ALL
+            SELECT MAX(browsernumber) + 1
+            FROM Browsers
+            WHERE userID = {x}
+        ) AS Numbers
+        WHERE n NOT IN (SELECT browsernumber FROM Browsers WHERE userID = {x})
+        ORDER BY n";
+
+                using (SqlCommand command = new SqlCommand(sqlQuery, connection))
+                {
+                    // Führe die Abfrage aus und lese das Ergebnis
+                    using (SqlDataReader reader = command.ExecuteReader())
+                    {
+                        if (reader.Read())
+                        {
+                            // Das Ergebnis abrufen und anzeigen
+                            minBrowserNummer = Convert.ToInt32(reader[0]);
+                            Console.WriteLine($"Die niedrigste unbenutzte Browsernummer für UserID {x} ist: {minBrowserNummer}");
+                        }
+                        else
+                        {
+                            Console.WriteLine("Keine unbenutzten Browsernummern gefunden.");
+                        }
+                    }
+                }
+            }
 
 
             Browser newBrowser = new Browser()
             {
                 browsername = browserInfo.browsername,
-                browsernumber = minUnusedBrowserNumber,
+                browsernumber = minBrowserNummer,
                 userId = browserInfo.userId,
             };
             _context.Browsers.Add(newBrowser);
             _context.SaveChanges();
-            return Ok(minUnusedBrowserNumber);
+            return Ok(minBrowserNummer);
             
         }
 
